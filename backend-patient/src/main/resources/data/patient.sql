eb4052d7fbca02003ac49747342833023983146b
Matthieu Chollet <matthieuchollet@MacBook-Pro-de-Matthieu.local>
2024-08-10 17:31:20 +0200

    fix(patient): add serial primary key

--- backend-patient/src/main/resources/data/patient.sql
+++ backend-patient/src/main/resources/data/patient.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS patient
 (
-    id         INT PRIMARY KEY,
+    id         SERIAL PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL,
     last_name  VARCHAR(50) NOT NULL,
     birth_date DATE        NOT NULL,
