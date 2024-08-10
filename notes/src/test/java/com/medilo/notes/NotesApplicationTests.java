78f7275738b445bc1ae7f268a788185655853929
Matthieu Chollet <matthieuchollet@MacBook-Pro-de-Matthieu.local>
2024-08-10 17:36:28 +0200

    refactor(notes): remove unuse import

--- notes/src/test/java/com/medilo/notes/NotesApplicationTests.java
+++ notes/src/test/java/com/medilo/notes/NotesApplicationTests.java
@@ -2,7 +2,6 @@ package com.medilo.notes;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @DataMongoTest
