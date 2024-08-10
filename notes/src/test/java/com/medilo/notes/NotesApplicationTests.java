ddac911afb4507bba7130bf8df5a1e86755c65cc
Matthieu Chollet <matthieuchollet@MacBook-Pro-de-Matthieu.local>
2024-08-10 14:43:18 +0200

    test(notes): add test profile and embed data

--- notes/src/test/java/com/medilo/notes/NotesApplicationTests.java
+++ notes/src/test/java/com/medilo/notes/NotesApplicationTests.java
@@ -1,9 +1,12 @@
 package com.medilo.notes;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@DataMongoTest
+@ActiveProfiles("test")
 class NotesApplicationTests {
 
 	@Test
