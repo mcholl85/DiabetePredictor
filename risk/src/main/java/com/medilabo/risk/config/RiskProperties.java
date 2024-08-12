75a69886d79e008f899541cdfe3114878a9be64a
Matthieu Chollet <matthieuchollet@MacBook-Pro-de-Matthieu.local>
2024-08-12 11:40:51 +0200

    feat(risk): add refresh scope on medilabo properties

--- risk/src/main/java/com/medilabo/risk/config/RiskProperties.java
+++ risk/src/main/java/com/medilabo/risk/config/RiskProperties.java
@@ -3,12 +3,14 @@ package com.medilabo.risk.config;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
 
 @Configuration
 @ConfigurationProperties(prefix = "risk")
+@RefreshScope
 @Getter
 @Setter
 public class RiskProperties {
--- risk/src/main/java/com/medilabo/risk/config/WebClientConfig.java
+++ risk/src/main/java/com/medilabo/risk/config/WebClientConfig.java
@@ -1,11 +1,13 @@
 package com.medilabo.risk.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
+@RefreshScope
 public class WebClientConfig {
     @Value("${medilabo.api.url}")
     private String apiUrl;
