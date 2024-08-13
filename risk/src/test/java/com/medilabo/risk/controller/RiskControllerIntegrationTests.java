22e6ffb8f02e0364b09a7ea2020d8f5448afe49c
Matthieu Chollet <matthieuchollet@MacBook-Pro-de-Matthieu.local>
2024-08-14 00:54:35 +0200

    test(risk): add integration tests with mockWebServer

--- risk/src/test/java/com/medilabo/risk/controller/RiskControllerIntegrationTests.java
+++ risk/src/test/java/com/medilabo/risk/controller/RiskControllerIntegrationTests.java
@@ -1,37 +1,28 @@
 package com.medilabo.risk.controller;
 
-import com.medilabo.risk.constant.RiskLevel;
-import com.medilabo.risk.dto.NoteDto;
-import com.medilabo.risk.dto.PatientDto;
-import com.medilabo.risk.exception.PatientNotFoundException;
-import com.medilabo.risk.exception.ServerErrorException;
-import com.medilabo.risk.service.NotesService;
-import com.medilabo.risk.service.PatientService;
-import com.medilabo.risk.service.RiskService;
+import okhttp3.mockwebserver.Dispatcher;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import okhttp3.mockwebserver.RecordedRequest;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.time.LocalDate;
-import java.util.Collections;
-import java.util.List;
+import java.io.IOException;
 
 import static org.hamcrest.Matchers.is;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
@@ -40,34 +31,52 @@ class RiskControllerIntegrationTests {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
-    private PatientService patientService;
-
-    @MockBean
-    private NotesService notesService;
-
-    private PatientDto mockPatient;
-    private List<NoteDto> mockNotes;
+    private MockWebServer mockWebServer;
 
     @BeforeEach
-    void setUp() {
-        mockPatient = new PatientDto();
-        mockPatient.setId(1);
-        mockPatient.setBirthDate(LocalDate.of(1966,12,31));
-        mockPatient.setGender("F");
+    void setUp() throws IOException {
+        if (mockWebServer == null) {
+            mockWebServer = new MockWebServer();
+            mockWebServer.start(8080);
+        }
 
-        NoteDto note = new NoteDto();
-        note.setPatId(1);
-        note.setNote("Le patient déclare qu'il 'se sent très bien' Poids égal ou inférieur au poids recommandé");
+        Dispatcher dispatcher = new Dispatcher() {
+            @NotNull
+            @Override
+            public MockResponse dispatch(RecordedRequest request) {
+                String path = request.getPath();
 
-        mockNotes = List.of(note);
+                assert path != null;
+                return switch (path) {
+                    case "/patients/1" -> new MockResponse()
+                            .setBody("{\"id\":1,\"birthDate\":\"1966-12-31\",\"gender\":\"F\"}")
+                            .addHeader("Content-Type", "application/json");
+                    case "/patients/2" -> new MockResponse()
+                            .setBody("{\"id\":2,\"birthDate\":\"1970-01-01\",\"gender\":\"M\"}")
+                            .addHeader("Content-Type", "application/json");
+                    case "/patients/3" -> new MockResponse()
+                            .setResponseCode(500);
+                    case "/patients/1/notes" -> new MockResponse()
+                            .setBody("[{\"patId\":1,\"note\":\"Le patient déclare qu'il 'se sent très bien' Poids égal ou inférieur au poids recommandé\"}]")
+                            .addHeader("Content-Type", "application/json");
+                    case "/patients/2/notes", "/patients/3/notes", "/patients/4/notes" -> new MockResponse()
+                            .setBody("[]")
+                            .addHeader("Content-Type", "application/json");
+                    default -> new MockResponse().setResponseCode(404);
+                };
+            }
+        };
+        mockWebServer.setDispatcher(dispatcher);
+    }
+
+    @AfterEach
+    void tearDown() throws IOException {
+        mockWebServer.shutdown();
     }
 
     @Test
+    @WithMockUser
     void testGetRiskById_Success() throws Exception {
-        when(patientService.fetchPatientById(anyInt())).thenReturn(mockPatient);
-        when(notesService.fetchNotesByPatId(anyInt())).thenReturn(mockNotes);
-
         mockMvc.perform(get("/risk/1")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
@@ -76,33 +85,27 @@ class RiskControllerIntegrationTests {
     }
 
     @Test
+    @WithMockUser
     void testGetRiskById_PatientNotFound() throws Exception {
-        when(patientService.fetchPatientById(anyInt()))
-                .thenThrow(new PatientNotFoundException("Patient not found"));
-
-        mockMvc.perform(get("/risk/1")
+        mockMvc.perform(get("/risk/4")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isNotFound())
-                .andExpect(jsonPath("$.message", is("Patient not found")));
+                .andExpect(jsonPath("$.message", is("Patient with ID 4 not found.")));
     }
 
     @Test
+    @WithMockUser
     void testGetRiskById_ServerError() throws Exception {
-        when(notesService.fetchNotesByPatId(anyInt()))
-                .thenThrow(new ServerErrorException("Server notes error"));
-
-        mockMvc.perform(get("/risk/1")
+        mockMvc.perform(get("/risk/3")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isInternalServerError())
-                .andExpect(jsonPath("$.message", is("Server notes error")));
+                .andExpect(jsonPath("$.message", is("Server patient error.")));
     }
 
     @Test
+    @WithMockUser
     void testGetRiskById_EmptyNotes() throws Exception {
-        when(patientService.fetchPatientById(anyInt())).thenReturn(mockPatient);
-        when(notesService.fetchNotesByPatId(anyInt())).thenReturn(Collections.emptyList());
-
-        mockMvc.perform(get("/risk/1")
+        mockMvc.perform(get("/risk/2")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.level", is("NONE")));
